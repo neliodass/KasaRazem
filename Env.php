424bd92998321a893e08108fd931e3dc7a5832d1
Bartek Pieróg <pierogbartoszzstti@gmail.com>
2025-12-01 19:53:15 +0100

    add Database and Env classes for database connection management and environment variable loading

---
+++ Env.php
@@ -0,0 +1,22 @@
+<?php
+
+class Env
+{
+    public static function load(string $path)
+    {
+        if (!file_exists($path)) {
+            return;
+        }
+
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            if (str_starts_with($line, '#')) {
+                continue;
+            }
+            list($name, $value) = explode('=', $line, 2);
+            $_ENV[$name] = $value;
+            putenv("$name=$value");
+        }
+    }
+}
