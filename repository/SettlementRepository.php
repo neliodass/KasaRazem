1aa869aab71e981eea57443654f1aa1468589510
Bartek Pieróg <pierogbartoszzstti@gmail.com>
2025-12-10 23:21:12 +0100

    add settlement functionality with details view and debt settling process

--- repository/SettlementRepository.php
+++ repository/SettlementRepository.php
@@ -11,4 +11,24 @@ class SettlementRepository extends Repository
         }
         return self::$instance;
     }
+
+    public function addSettlement(int $fromUserId, int $toUserId, float $amount, int $groupId, string $date): ?int
+    {
+        $query = $this->conn->prepare(
+            'INSERT INTO settlements (group_id,payer_user_id,payee_user_id, amount, date_settled) 
+                 VALUES (:groupId,:fromUserId, :toUserId, :amount,  :date)
+                 RETURNING id'
+        );
+
+        $query->bindParam(':fromUserId', $fromUserId, PDO::PARAM_INT);
+        $query->bindParam(':toUserId', $toUserId, PDO::PARAM_INT);
+        $query->bindParam(':amount', $amount);
+        $query->bindParam(':groupId', $groupId, PDO::PARAM_INT);
+        $query->bindParam(':date', $date);
+        $query->execute();
+
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        return $result ? (int)$result['id'] : null;
+    }
+
 }
--- src/controllers/BalanceController.php
+++ src/controllers/BalanceController.php
@@ -2,12 +2,14 @@
 
 require_once "src/services/BalanceService.php";
 require_once "src/services/GroupService.php";
+require_once "repository/SettlementRepository.php";
 
 class BalanceController extends AppController
 {
     private static $instance = null;
     private GroupService $groupService;
     private BalanceService $balanceService;
+    private SettlementRepository $settlementRepository;
 
     public static function getInstance()
     {
@@ -21,6 +23,7 @@ class BalanceController extends AppController
     {
         $this->groupService = GroupService::getInstance();
         $this->balanceService = BalanceService::getInstance();
+        $this->settlementRepository = SettlementRepository::getInstance();
     }
 
     public function balance($groupId)
@@ -38,5 +41,60 @@ class BalanceController extends AppController
         ]);
         exit();
     }
+
+    public function settleDetails($groupId)
+    {
+        Auth::requireLogin();
+        $groupId = (int)$groupId;
+        $currentUserId = (int)Auth::userId();
+        $balanceData = $this->balanceService->getBalanceSummary((string)$groupId, (string)$currentUserId);
+        $settlements = $this->balanceService->calculateSettlements($balanceData['balance']);
+        $usersInGroup = $this->groupService->getUsersInGroup($groupId);
+        $users = array_column($usersInGroup, 'firstname', 'id');
+        $settlementsNamed = [];
+        foreach ($settlements as $settlement) {
+            $settlement['from_name'] = $users[$settlement['from']];
+            $settlement['to_name'] = $users[$settlement['to']];
+            $settlementsNamed[] = $settlement;
+        }
+
+        $this->render('settlementsDetails', [
+            "activeTab" => "balance",
+            "groupName" => ($this->groupService)->getGroupName((string)$groupId),
+            "groupId" => $groupId,
+            "settlements" => $settlementsNamed,
+            "usersInGroup" => $usersInGroup
+        ]);
+        exit();
+    }
+    public function settleDebt($groupId)
+    {
+        if(!$this->isPost()) {
+            header("Location: /groups/$groupId/balance");
+            exit();
+        }
+        Auth::requireLogin();
+        $groupId = (int)$groupId;
+        $currentUserId = (int)Auth::userId();
+        $payerId = (int)$_POST['payer_id']??0;
+        $payeeId = (int)$_POST['payee_id']??0;
+        $amount = (float)$_POST['amount']??0.0;
+        if($payerId !== $currentUserId && $payeeId !== $currentUserId) {
+            header("Location: /groups/$groupId/balance");
+            exit();
+        }
+        if($amount >0 && $payerId !== $payeeId) {
+            $this->settlementRepository->addSettlement(
+                $payerId,
+                $payeeId,
+                $amount,
+                $groupId,
+                date('Y-m-d')
+            );
+        }
+        $this->redirect('/groups/' . $groupId . '/settlements');
+        exit();
+    }
+
 
 }
